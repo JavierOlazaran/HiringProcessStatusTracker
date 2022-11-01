1610a6c90520ced2e9158ca894e25e8fb9528f7f
jjolazaran <jjolazaran@gmail.com>
2022-11-01 19:19:14 -0300

    Created initial app structure.

--- ui/web/HiringProcessStatusTrackerWeb/src/app/app-routing.module.ts
+++ ui/web/HiringProcessStatusTrackerWeb/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', loadChildren: () => import('./pages/main/main.module').then(m => m.MainModule) },
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
+  { path: '**', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

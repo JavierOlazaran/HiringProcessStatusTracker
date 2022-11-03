ba257c26be1f4d826bd42ae5859b7bc4d590cede
jjolazaran <jjolazaran@gmail.com>
2022-11-02 23:47:34 -0300

    Added Angular Material

--- ui/web/HiringProcessStatusTrackerWeb/src/app/app-routing.module.ts
+++ ui/web/HiringProcessStatusTrackerWeb/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full'},
   { path: 'main', loadChildren: () => import('./pages/main/main.module').then(m => m.MainModule) },
   { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
   { path: '**', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) }
